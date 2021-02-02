03f51a779170ca86d88cf05f295f697e72470cf5
roy <friedroyze@gmail.com>
2021-02-02 20:17:26 +0800

    changed management layout to have navbar

--- app/src/main/java/com/example/MCLShuttleTracker/SettingsActivity.java
+++ app/src/main/java/com/example/MCLShuttleTracker/SettingsActivity.java
@@ -1,15 +1,21 @@
 package com.example.MCLShuttleTracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class SettingsActivity extends AppCompatActivity {
 
     Button btnEditProfile, btnDestinations, btnTransits, btnSchedules;
+    BottomNavigationView navManagement;
 
     String driverId;
 
@@ -18,47 +24,52 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        navManagement = findViewById(R.id.navManagement);
+        NavController navController = Navigation.findNavController(this,  R.id.frgManagement);
 
-        btnEditProfile = findViewById(R.id.btnEditProfile);
-        btnDestinations = findViewById(R.id.btnDestinations);
-        btnSchedules = findViewById(R.id.btnSchedules);
-        btnTransits = findViewById(R.id.btnTransits);
+        NavigationUI.setupWithNavController(navManagement, navController);
 
-        driverId = getIntent().getStringExtra("driverId");
 
-        btnDestinations.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SettingsActivity.this, DestinationsActivity.class);
-                startActivity(intent);
-            }
-        });
+//        btnEditProfile = findViewById(R.id.btnEditProfile);
+//        btnDestinations = findViewById(R.id.btnDestinations);
+//        btnSchedules = findViewById(R.id.btnSchedules);
+//        btnTransits = findViewById(R.id.btnTransits);
 
-        btnTransits.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SettingsActivity.this, DestinationsActivity.class);
-                intent.putExtra("driverId", driverId);
-                startActivity(intent);
-            }
-        });
+//        driverId = getIntent().getStringExtra("driverId");
 
-        btnEditProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SettingsActivity.this, AccountEditActivity.class);
-                intent.putExtra("driverId", driverId);
-                startActivity(intent);
-            }
-        });
-
-        btnSchedules.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SettingsActivity.this, SchedulesActivity.class);
-                startActivity(intent);
-            }
-        });
+//        btnDestinations.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(SettingsActivity.this, DestinationsActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        btnTransits.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(SettingsActivity.this, DestinationsActivity.class);
+//                intent.putExtra("driverId", driverId);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        btnEditProfile.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(SettingsActivity.this, AccountEditActivity.class);
+//                intent.putExtra("driverId", driverId);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        btnSchedules.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(SettingsActivity.this, SchedulesActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
     }
 }
